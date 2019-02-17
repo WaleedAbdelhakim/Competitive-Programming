e7ef25fa320f43195f53d87ecd7f47b5a3689875
WaleedAbdelhakim <waleedsadek98@gmail.com>
2019-02-17 08:53:29 +0300

    explanation added

--- others/FbHkrCup-18-R1-A.cpp
+++ others/FbHkrCup-18-R1-A.cpp
@@ -1,3 +1,20 @@
+// observations :
+// 1 - # of columns has to be even
+// 2 - the only case we have more than one choice 
+// is if the flow is at a cell in the second row
+// and you got to it through it's left side 
+// choices are gonna be up and down
+// and whatever choice you make you will have only one choice 
+// until you reach a similar cell
+// so if you are at (2, x) and you got to it through (2, x - 1)
+// you are gonna have at most two ways to get to (2 , x + 2)
+// and you can count them by checking if there is an obstacle in either of them
+// let w[x] be a number >= 0 and <= 2 and it denotes
+// the number of ways to go from x to x + 2
+// the final answer is w[2] * w[4] * w[6] * ... * w[n]
+// and as we start our calculations from col 2
+// we have to make sure we can reach it in the first place 
+
 #include <bits/stdc++.h>
 
 using namespace std;
