837eb8b44a887825158f04756dc4bd77dbb11e63
WaleedAbdelhakim <waleedsadek98@gmail.com>
2019-03-09 03:13:26 +0200

    explanation added

--- TopCoder/SRM392-D1-1000.cpp
+++ TopCoder/SRM392-D1-1000.cpp
@@ -1,3 +1,26 @@
+/*
+   explanation :
+   i use boolean dp with state i(int) , g(bool) , f(vector<int>)
+   it means , is there a way to fill the rightmost i + 1 digits knowing that 
+   the number I'm currently forming is already  greater than the input if 
+   g is 1 or still equal to it if g is 0, and having the frequency of each digit
+   stored in f.
+
+   the reason why this passes is that I ignore subproblems that lead to invalid freq       arrays , and the number of valid freq arrays is small.
+   a valid freq array can't have a sum of frequencies greater than 19,
+   and if we increased every non zero frequency to be equal to the maximum
+   the sum of added values must be smaller than or equal to i + 1
+   
+   e.g. if the frequency array looks like this : 1 0 0 1 2 1 0 0 0 7 and i is 8
+   the only hope for this number to be equadigit is for digits to equal at least 7
+   21 digits needed and only 8 remaining, so that is not a valid freq array
+
+
+   after that, we build the answer, if there was more than a choice you can put 
+   in the current digit that leads to an equadigit number 
+   go for the lowest one
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,12 +31,13 @@ const int N = 20;
 map<vi , bool> memo[N][2];
 class EquiDigitNumbers {
     string s;
-    bool valid (vi &v) {
-        int len = *max_element(v.begin() , v.end()) * (v.size() - count(v.begin() , v.end() , 0));
-        return len < N;
+    bool valid (int i , vi &v) {
+        int mx = *max_element(v.begin() , v.end()) , rem = 0;
+        for (int x : v) if (x) rem += mx - x;
+        return rem <= i + 1;
     }
 
-    bool good(vi &v) {
+    bool is_equa(vi &v) {
         int mx = *max_element(v.begin() , v.end());
         for (int x : v)
             if (x != mx && x != 0)
@@ -22,8 +46,8 @@ class EquiDigitNumbers {
     }
 
     int solve (int i , bool g , vi f) {
-        if (!valid(f)) return 0;
-        if (i < 0) return good(f);
+        if (!valid(i , f)) return 0;
+        if (i < 0) return is_equa(f);
 
         if (memo[i][g].count(f))
             return memo[i][g][f];
@@ -62,3 +86,8 @@ public:
         return stoll(build(N - 2 , 0 , vi(10 , 0)));
     }
 };
+
+int main() {
+    EquiDigitNumbers test;
+    cout << test.findNext("48");
+}
