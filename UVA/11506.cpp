3a707aa76f47f6088fb117417b137bdb00637516
WaleedAbdelhakim <waleedsadek98@gmail.com>
2019-03-31 13:56:25 +0200

    new solution added

---
+++ UVA/11506.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct edge {
+    int to , cap;
+};
+
+const int N = 205; // make sure is greater than max_node will ever be
+int dist[N] , p[N] , src , max_node , snk; // don't forget to set src and snk
+vector<edge> edges;
+vector<int> adj[N];
+
+void addAugEdge(int u , int v , int c) {
+    max_node = max(max_node , max(u , v));
+    adj[u].push_back(edges.size());
+    edges.push_back({v , c});
+    adj[v].push_back(edges.size());
+    edges.push_back({u , 0}); // c if undirected 0 if directed
+}
+
+bool bfs() {
+    memset(dist , -1 , (max_node + 1) * sizeof dist[0]);
+    queue<int> q;
+    q.push(src);
+    dist[src] = 0;
+    while (q.size()) {
+        int cur = q.front();
+        q.pop();
+        for (int ci : adj[cur]) {
+            auto &e = edges[ci];
+            if (dist[e.to] == -1 && e.cap) {
+                q.push(e.to);
+                dist[e.to] = dist[cur] + 1;
+            }
+        }
+    }
+    return dist[snk] != -1;
+}
+
+int dfs (int node , int flow) {
+    if (node == snk || !flow)
+        return flow;
+
+    for (; p[node] < adj[node].size() ; p[node]++) {
+        auto &e= edges[adj[node][p[node]]];
+        auto &re = edges[adj[node][p[node]]^1];
+
+        if (dist[node] + 1  != dist[e.to]) continue;
+
+        int push = dfs(e.to , min(flow , e.cap));
+        if (push) {
+            e.cap -= push;
+            re.cap += push;
+            return push;
+        }
+    }
+
+    return 0;
+}
+
+long long dinic() {
+    long long flow = 0;
+    while (bfs()) {
+        memset(p , 0 , sizeof p);
+        while (int f = dfs(src , 1e9)) // make sure that no edge has cap greater than 1e9
+            flow += f;
+    }
+    return flow;
+}
+
+void init() { // every test case
+    edges.clear();
+    for (int i = 0 ;i <= max_node;i++)
+        adj[i].clear();
+    max_node = 0;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.in" , "r" , stdin);
+#endif
+    int n , m , u , v , c , b , d;
+    while (scanf("%d" , &n) == 1) {
+        init();
+
+        src = N - 1 , snk = N - 2;
+        for (int i = 1 ;i <= n ;i++) {
+            scanf("%d" , &c);
+            addAugEdge(i , i + n , c);
+        }
+
+        scanf("%d" , &m);
+        while (m--) {
+            scanf("%d%d%d" , &u , &v , &c);
+            addAugEdge(u + n , v , c);
+        }
+
+        scanf("%d%d" , &b , &d);
+        while (b--) {
+            scanf("%d" , &u);
+            addAugEdge(src , u , 1e9);
+        }
+
+        while (d--) {
+            scanf("%d" , &v);
+            addAugEdge(v + n , snk , 1e9);
+        }
+
+        printf("%lld\n" , dinic());
+    }
+}
