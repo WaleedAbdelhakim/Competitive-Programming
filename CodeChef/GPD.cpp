fd826e8ec078e832a4324fa32def82f25390b95e
WaleedAbdelhakim <47154793+WaleedAbdelhakim@users.noreply.github.com>
2020-01-05 21:39:25 +0200

    Update GPD.cpp

--- CodeChef/GPD.cpp
+++ CodeChef/GPD.cpp
@@ -7,61 +7,61 @@ map<long long , long long> k;
 map<long long , vector<long long> > adj;
 
 struct node {
-    long long sum = 0;
-    node *left, *right;
-    node(long long sum, node *left, node *right) : sum(sum), left(left), right(right) {}
-    node(long long sum = 0) : sum(sum), left(this), right(this) {}
+    int sum = 0;
+    node *zero, *one;
+    node(int sum, node *zero, node *one) : sum(sum) , zero(zero) , one(one) {}
+    node(int sum = 0) : sum(sum), one(this), zero(this) {}
 };
 
-node *insert(node *cur, long long v , long long i = 31) {
+node *insert(node *cur, int v , int i = 31) {
     if (i == 0)
         return new node(cur->sum + 1);
 
-    node *left = cur->left;
-    node *right = cur->right;
+    node *zero = cur->zero;
+    node *one = cur->one;
 
-    long long j = i - 1;
-    if (v & (1ll << j))
-        left = insert(cur->left , v , j);
+    int j = i - 1;
+    if (v & (1 << j))
+        one = insert(cur->one , v , j);
     else
-        right = insert(cur->right , v , j);
+        zero = insert(cur->zero , v , j);
 
-    return new node(cur->sum + 1, left, right);
+    return new node(cur->sum + 1, zero, one);
 }
 
 map<long long , node*> roots;
 
-long long get_max(node *cur , long long v , long long i = 31) {
+int get_max(node *cur , int v , int i = 31) {
     if (i == 0)
         return 0;
 
-    long long j = i - 1;
-    if (v & (1ll << j)) {
-        if (cur->right->sum) return (1ll << j) + get_max(cur->right , v , j);
-        return get_max(cur->left , v , j);
+    int j = i - 1;
+    if (v & (1 << j)) {
+        if (cur->zero->sum) return (1 << j) + get_max(cur->zero , v , j);
+        return get_max(cur->one , v , j);
     }
 
-    if (cur->left->sum) return (1ll << j) + get_max(cur->left , v , j);
-    return get_max(cur->right , v , j);
+    if (cur->one->sum) return (1 << j) + get_max(cur->one , v , j);
+    return get_max(cur->zero , v , j);
 }
 
-long long get_min(node *cur , long long v , long long i = 31) {
+int get_min(node *cur , int  v , int i = 31) {
     if (i == 0)
         return 0;
 
-    long long j = i - 1;
-    if (v & (1ll << j)) {
-        if (cur->left->sum) return get_min(cur->left , v , j);
-        return (1ll << j) + get_min(cur->right , v , j);
+    int j = i - 1;
+    if (v & (1 << j)) {
+        if (cur->one->sum) return get_min(cur->one , v , j);
+        return (1 << j) + get_min(cur->zero , v , j);
     }
 
-    if (cur->right->sum) return get_min(cur->right , v , j);
-    return (1ll << j) + get_min(cur->left, v , j);
+    if (cur->zero->sum) return get_min(cur->zero , v , j);
+    return (1 << j) + get_min(cur->one, v , j);
 }
 
-void dfs(long long node , long long par) {
+void dfs(int node , int par) {
     roots[node] = insert(roots[par] , k[node]);
-    for (long long child : adj[node])
+    for (int child : adj[node])
         if (child != par)
             dfs(child , node);
 }
@@ -71,12 +71,12 @@ int main() {
 #ifndef ONLINE_JUDGE
     freopen("input.in" , "r" , stdin);
 #endif
-    long long n , q , r ;
+    int  n , q , r ;
     cin >> n >> q >> r;
     cin >> k[r];
 
-    long long u , v;
-    for (long long i = 0 ;i < n - 1 ;i++) {
+    int  u , v;
+    for (int i = 0 ;i < n - 1 ;i++) {
         cin >> v >> u;
         cin >> k[v];
         adj[u].push_back(v);
@@ -85,22 +85,21 @@ int main() {
     roots[r] = new node();
     dfs(r , r);
 
-    long long last = 0 ;
-    long long type;
+    int last = 0 , type;
     while (q--) {
         cin >> type;
         type ^= last;
         if (type) {
-            long long u , val;
+            int u , val;
             cin >> u >> val;
             u ^= last;
             val ^= last;
-            long long mn = get_min(roots[u] , val);
-            long long mx = get_max(roots[u] , val);
+            int mn = get_min(roots[u] , val);
+            int mx = get_max(roots[u] , val);
             cout << mn << " " << mx << '\n';
             last = mn ^ mx;
         } else {
-            long long u , v;
+            int u , v;
             cin >> u >> v;
             u ^= last;
             v ^= last;
